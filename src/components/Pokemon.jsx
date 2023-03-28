29110ab16b1dac7c72164fe2b7a2a4bcfcc849a0
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-03-28 10:35:05 +0200

    first pokement

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,12 @@
+
+
+function Bulbasaur(){
+
+return <figure>
+    <img src="C:\Users\guill\desktop\wild\pokedex\Pokedex\src\images\bulbizar.png"
+         alt="bulbasaur">
+    <figcaption>Bulbasaur</figcaption>
+</figure>;
+}
+
+export default Bulbasaur;
