37d2102c14194e8a55115cbb0d5848e2a220dc2f
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-03-28 21:37:28 +0200

    quest 3 final branching

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,20 @@
 function PokemonCard() {
-  const pokemon = pokemonList[0];
-  if (pokemon)
+  const pokemon = pokemonList[1];
+  if (pokemon.imgSrc) {
     return (
       <figure className="card">
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-          className="card-img"
-        />
-        <figcaption>Bulbasaur</figcaption>
+        <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img" />
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     );
+  } else {
+    return (
+      <div>
+        <p>???</p>
+        <figcaption>{pokemon.name}</figcaption>
+      </div>
+    );
+  }
 }
 
 const pokemonList = [
