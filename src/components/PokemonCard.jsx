77835d33967e07ed43f3f50a19573d7fa4ed017c
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-03-28 20:03:11 +0200

    quest 3 update

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,34 +1,27 @@
-const card = {
-  width: '200px',
-  padding: '1rem',
-  backgroundColor: 'lightgreen',
-  borderRadius: '5px',
-  boxShadow: '0px 0px 5px gray',
-  color: 'white',
-  fontSize: '1.6rem',
-  textAlign: 'center',
-  textShadow: '0px 0px 5px gray',
-};
-
 function PokemonCard() {
-  return (
-    <figure style={card}>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-        className="card-img"
-        style={{
-          width: '100%',
-          height: 'auto',
-          marginBottom: '0.5rem',
-          backgroundColor: 'white',
-          borderRadius: '15px',
-          boxShadow: 'inherit',
-        }}
-      />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
+  const pokemon = pokemonList[0];
+  if (pokemon)
+    return (
+      <figure className="card">
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="bulbasaur"
+          className="card-img"
+        />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    );
 }
+
+const pokemonList = [
+  {
+    name: 'bulbasaur',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'mew',
+  },
+];
 
 export default PokemonCard;
