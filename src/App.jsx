0ab3e967b5da7affd94f07f9389f95e61ef03d0d
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-03-29 17:23:26 +0200

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,5 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
+import { useState } from 'react';
 import './App.css';
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>s
-//   )
-// }
-
 import PokemonCard from './components/PokemonCard';
 
 const pokemonList = [
@@ -41,16 +9,48 @@ const pokemonList = [
       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
   },
   {
+    name: 'charmander',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
+  },
+  {
+    name: 'squirtle',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
+  },
+  {
+    name: 'pikachu',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+  },
+  {
     name: 'mew',
   },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClick = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
   return (
     <div>
+      <button onClick={handleClick}>Précédent</button>
       <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
+      <button onClick={handleClick}>Suivant</button>
     </div>
   );
 }
 
 export default App;
+
+// const [count, setCount] = useState(0);
+
+//   const handleClick = () => {
+//     setCount(count + 1)
+//   }
+
+//   return (<div>
+//     <p>{count}</p>
+//     <button onClick={handleClick}>Click</button>
+//   </div>);
